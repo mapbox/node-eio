754afcbd0d1bbb33716cb6f6dd80300dff7e9baa
Dane Springmeyer <dane@dbsgeo.com>
2013-04-24 15:34:38 -0700

    full test coverage

--- test/threads.test.js
+++ test/threads.test.js
@@ -1,10 +1,24 @@
 var eio = require('..');
+var assert = require('assert');
 
 describe('threads', function() {
-    it('should allow setting the number of threads', function() {
+    it('should allow setting the min number of threads', function() {
         // Increase number of threads to 1.5x the number of logical CPUs.
         var cpus = require('os').cpus().length;
         var threads = Math.ceil(Math.max(4, cpus* 1.5));
         eio.setMinParallel(threads);
+    });
+
+    it('should allow setting the max number of threads', function() {
+        // Increase number of threads to 1.5x the number of logical CPUs.
+        var cpus = require('os').cpus().length;
+        var threads = Math.ceil(Math.max(4, cpus* 1.5));
+        eio.setMaxParallel(threads);
+    });
+
+    it('should allow access to usage', function() {
+        assert.ok(eio.usage().requests !== undefined);
+        assert.ok(eio.usage().active !== undefined);
+        assert.ok(eio.usage().pending !== undefined);
     });
 });
